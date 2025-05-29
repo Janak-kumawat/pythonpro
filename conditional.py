b242cfa023abac092f8df822998c35bffc36e316
Janak-kumawat <kumawatjanak0806@gmail.com>
2025-05-29 14:59:22 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,12 @@
+light="blue"
+if(light=="green"):
+    print("go")
+
+elif(light=="red"):
+    print("stop")
+      
+elif(light=="yellow"):
+    print("ready")
+
+else:
+    print("no traffic lights")
---
+++ conditional2.py
@@ -0,0 +1,11 @@
+val1=int(input("enter a integer value"))
+val2=int(input("enter an anather value"))
+print("value one is :",val1)
+print("value two is :",val2)
+ 
+if(val1>val2):
+  print("value one is greater then value tow and the value of one is =",val1)
+elif(val2<val1):
+   print("valve two is greater then value one and the value of two is =",val2)
+else:
+  print("value one and two are equls")
