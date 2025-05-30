b15a7b3da1749a0aa767a2b711994f2d2ab1e740
Janak-kumawat <kumawatjanak0806@gmail.com>
2025-05-30 14:24:21 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+dict={
+    
+    "name":"janak",
+    "age": 39,
+    "marks": 78.78,
+    "subjects":["java","php"],
+    "collage":"swpg",
+    "friends":{
+        "Rahul":1,
+        "Aman":2,
+        "Rl":3,
+        "kamal":4,
+    }
+}
+print(dict)
+print(len(dict))
+print(type(dict))
+print(dict["name"])
---
+++ whileloop.py
@@ -0,0 +1,6 @@
+nums = [2,4,6,8,10,12,14,16]
+i=0
+while i<len(nums):
+    print([i])
+    i+=1
+
